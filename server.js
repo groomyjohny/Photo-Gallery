7723a8799f3f299778eaa3b0b930d659517e0ed3
groomyjohny <groomyjohny@gmail.com>
2021-03-10 13:33:35 +0500

    Created upload form
    Not yet usable

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const express = require('express');
 require('console-stamp')(console, 'dd.mm.yyyy HH:MM:ss.l');
 
 const port = 8082;
+const app = express();
 
 app.get('/', (req,res)=>{
     res.render("index.hbs");
+});
+
+app.get('/upload', (req,res) => {
+    res.render("upload.hbs");
 });
 
 app.use('/public',express.static('public'));
