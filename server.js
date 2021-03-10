4268fb4759cf147a450279827a21dafc71032009
GHKyrie <nemasterya@gmail.com>
2021-03-10 19:48:00 +0500

    Added package.json

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ require('console-stamp')(console, 'dd.mm.yyyy HH:MM:ss.l');
 const port = 8082;
 const app = express();
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render("index.hbs");
 });
 
-app.get('/upload', (req,res) => {
+app.get('/upload', (req, res) => {
     res.render("upload.hbs");
 });
-app.post('/upload',(req,res)=>{
-    
+app.post('/upload', (req, res) => {
+
 })
 
-app.use('/public',express.static('public'));
+app.use('/public', express.static('public'));
 
-app.listen(port, (req,res) => {
-    
+app.listen(port, (req, res) => {
+
 });
 
 const sqlConnectionData = {
